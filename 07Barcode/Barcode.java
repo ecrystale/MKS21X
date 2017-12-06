368717705ab859117d465ef2f86169e11aa589ed
Emily <elee19@stuy.edu>
2017-12-05 20:34:51 -0500

    improve barcode

--- 07Barcode/Barcode.java
+++ 07Barcode/Barcode.java
@@ -1,12 +1,16 @@
 import java.util.*;
 public class Barcode{
     private String zip;
+    private String zip6;
+    private static String[] nums={"1","2","3","4","5","6","7","8","9","0"};
+    private static String[] scheme={":::||","::|:|","::||:",":|::|",":|:|:",":||::","|:::|","|::|:","|:|::","||:::"};
     public Barcode(String ozip){
 	if (!isnumerical(ozip) && !(ozip.length()==5)){
 	    throw new IllegalArgumentException();
 	}
 	else{
-	    zip=ozip+checkvalue(ozip);
+	    zip=ozip;
+	    zip6=ozip+checkvalue(ozip);
 	}
     }
     private String checkvalue(String zipcode){
@@ -26,12 +30,12 @@ public class Barcode{
 	return true;
     }
 
-    public String toZip(String code){
+    public static String toZip(String code){
 	if (!(code.substring(0,1).equals("|")) || !(code.substring(code.length()-1,code.length()).equals("|"))){
 	    throw new IllegalArgumentException();
 	}
 	for (int i=0;i<code.length();i++){
-	    if (!(code.substring(i,i+1).equals("|")) || !(code.substring(i,i+1).equals(":"))){
+	    if (!((code.substring(i,i+1).equals("|")) || (code.substring(i,i+1).equals(":")))){
 		throw new IllegalArgumentException();
 	    }
 	}
@@ -39,44 +43,19 @@ public class Barcode{
 	    throw new IllegalArgumentException();
 	}
 	String finals="";
-	for (int i=1;i<code.length()-1;i++){ //takes out "|"
-	    String x=code.substring(i,i+1);
-	    if (x.equals(":::||")){
-		finals+="1"; 
-	    }
-	    if (x.equals("::|:|")){
-		finals+= "2";  
-	    }
-	    if (x.equals("::||:")){
-		finals+= "3";  
-	    }
-	    if (x.equals(":|::|")){
-		finals+= "4";  
-	    }
-	    if (x.equals(":|:|:")){
-		finals+= "5";  
-	    }
-	    if (x.equals(":||::")){
-		finals+= "6";  
-	    }
-	    if (x.equals("|:::|")){
-		finals+= "7";  
-	    }
-	    if (x.equals("|::|:")){
-		finals+= "8";  
-	    }
-	    if (x.equals("|:|::")){
-		finals+= "9";  
-	    }
-	    if (x.equals("||:::")){
-		finals+= "0";  
+	for (int i=1;i<code.length()-6;i+=5){ //takes out "|"
+	    String x=code.substring(i,i+5);
+	    for (int y=0;y<10;y++){
+		if (scheme[y].equals(x)){
+		    finals+=nums[y];
+		}
 	    }
 	}
 	return finals;
-     }
+    }
 
 
-    public String toCode(String zip){
+    public static String toCode(String zip){
 	Barcode a=new Barcode(zip);
 	return a.getCode();
     }
@@ -86,48 +65,23 @@ public class Barcode{
     }
 
     public int compareTo(Barcode other){
-        return getZip().compareTo(other.getZip());
+	return getZip().compareTo(other.getZip());
     }
 
     public String getCode(){
 	String finals="|";
-	for (int i=0;i<zip.length();i++){
-	    int x=Integer.valueOf(zip.substring(i,i+1));
-	    if (x==1){
-		finals+= ":::||"; 
-	    }
-	    if (x==2){
-		finals+= "::|:|";  
-	    }
-	    if (x==3){
-		finals+= "::||:";  
-	    }
-	    if (x==4){
-		finals+= ":|::|";  
-	    }
-	    if (x==5){
-		finals+= ":|:|:";  
-	    }
-	    if (x==6){
-		finals+= ":||::";  
-	    }
-	    if (x==7){
-		finals+= "|:::|";  
-	    }
-	    if (x==8){
-		finals+= "|::|:";  
-	    }
-	    if (x==9){
-		finals+= "|:|::";  
-	    }
-	    if (x==0){
-		finals+= "||:::";  
+	for (int i=0;i<zip6.length();i++){
+	    String x=zip6.substring(i,i+1);
+	    for (int y=0;y<10;y++){
+		if (nums[y].equals(x)){
+		    finals+=scheme[y];
+		}
 	    }
 	}
 	finals+="|";
 	return finals;
     }
-
+	    
     public String getZip(){
 	return zip;
     }
