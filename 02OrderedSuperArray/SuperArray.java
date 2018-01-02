9ec4ad1849de288d1f4fb31e5ad33a7b95f9a81d
ecrystale <elee19@stuy.edu>
2018-01-02 09:26:42 -0500

    fix OrderedSuperArray's SuperArray

--- 02OrderedSuperArray/SuperArray.java
+++ 02OrderedSuperArray/SuperArray.java
@@ -1,149 +1,157 @@
-class SuperArray{
+public class SuperArray{
+    
     private String[] data;
     private int size;
+
     public SuperArray(){
-	size=10;
-	data= new String[10];
+	data = new String[10];
+	size = 0;
     }
+
     public SuperArray(int startingCapacity){
-	size=startingCapacity;
-	data=new String[startingCapacity];
+	data = new String[startingCapacity];
+	size = 0;
     }
+
     public void clear(){
-	data= new String[10];
-	size=10;
+	size = 0;
+	String[] data2 = new String[size];
+	data = data2;
     }
+
     public int size(){
 	return size;
     }
+
+    public boolean isEmpty(){
+	return size == 0;
+    }
+
     public boolean add(String element){
-	size++;
-	String [] n=new String[size];
-	for (int i=0; i<n.length-1;i++){
-	    n[i]=data[i];
+	if (data.length == size()){
+	    resize();
 	}
-	n[size-1]=element;
-	data=n;
-	return (data[size-1]).equals(element);
+	String[] data2 = new String[data.length];
+	for (int x = 0; x <= size(); x++){
+	    data2[x] = data[x];
+	}
+	data2[size()] = element;
+	data = data2;
+	size = size + 1;
+	return true;
     }
+
     public String toString(){
-	String line="[";
-	for(int i=0;i<size-1;i++){
-	    line+=data[i]+",";
+	String ans = "[";
+	for (int x = 0; x < size - 1; x++){
+	    ans = ans + data[x] + ", ";
 	}
-	line+=data[size-1]+"]";
-	return line;
+	ans = ans + data[size - 1] + "]";
+	return ans;
     }
+
     public String get(int index){
-	if (index < 0 || index > size){
-	    System.out.println("index is out of range");
-	    return null;
+	if (index < 0 || index >= size()){
+	    throw new IndexOutOfBoundsException();
 	}
-	else{
-	    return data[index];
-	}
+	return data[index];
     }
+
     public String set(int index, String element){
-	if (index < 0 || index > size){
-	    System.out.println("index is out of range");
-	    return null;
+	if (index < 0 || index >= size()){
+	    throw new IndexOutOfBoundsException();
 	}
 	else{
-	    String a=data[index];
-	    data[index]=element;
-	    return a;
+	    String old = data[index];
+	    data[index] = element;
+	    return old;
 	}
     }
 
-    // phase 2
-    public void resize(){
-	String []copy= new String[2*size];
-	for (int i=0; i<size; i++){
-	    copy[i]=data[i];
+    private void resize(){
+	String[] data2 = new String[size() + size() + 1];
+	for (int x = 0; x < size(); x++){
+	    data2[x] = data[x];
 	}
-	data=copy;
-	size=size*2;
+	data = data2;
     }
 
-    //Phase 3
-    public boolean contains(String target){
-	for (int i=0;i<size;i++){
-	    if (data[i]==target){
+    public boolean contains(String element){
+	for (int x = 0; x < size(); x++){
+	    if (data[x].equals(element)){
 		return true;
 	    }
 	}
 	return false;
     }
-    public int indexOf(String target){
-	for (int i=0;i<size;i++){
-	    if (data[i]==target){
-		return i;
+
+    public int indexOf(String element){
+	for (int x = 0; x < size(); x++){
+	    if (data[x].equals(element)){
+		return x;
 	    }
 	}
 	return -1;
     }
-    public int lastIndexOf(String target){
-	for (int i=size-1;i>-1;i--){
-	    if (data[i]==target){
-		return i;
+
+    public int lastIndexOf(String element){
+	for (int x = size() - 1; x >= 0; x--){
+	    if (data[x].equals(element)){
+		return x;
 	    }
 	}
 	return -1;
     }
 
     public void add(int index, String element){
-	if (index < 0 || index > size){
-	    System.out.println("index is out of range");
+	if (index < 0 || index > size()){
+	    throw new IndexOutOfBoundsException();
 	}
-	else{
-	    String[] arr=new String[size+1];
-	    for (int i=0;i<index;i++){
-		arr[i]=data[i];
-	    }
-	    arr[index]=element;
-	    for (int i=index;i<size;i++){
-		arr[i+1]=data[i];
-	    }
-	    data=arr;
-	    size++;
+	if (data.length == size()){
+	    resize();
 	}
+	String[] data2 = new String[data.length];
+	for (int x = 0; x < index; x++){
+	    data2[x] = data[x];
+	}
+	data2[index] = element;
+	for (int x = index; x < size(); x++){
+	    data2[x + 1] = data[x];
+	}
+	data = data2;
+	size = size + 1;
     }
 
     public String remove(int index){
-	if (index < 0 || index > size){
-	    System.out.println("index is out of range");
-	    return null;
+	if (index < 0 || index > size()){
+	    throw new IndexOutOfBoundsException();
 	}
-	else{
-	    String item=data[index];
-	    String[] arr=new String[size-1];
-	    for (int i=0;i<index;i++){
-		arr[i]=data[i];
-	    }
-	    for (int i=index+1;i<size;i++){
-		arr[i-1]=data[i];
-	    }
-	    data=arr;
-	    size--; 
-	    return item;
+	String old = data[index];
+	for (int x = index; x < size() - 1; x++){
+	    data[x] = data[x+1];
 	}
+	size = size - 1;
+	return old;
     }
 
     public boolean remove(String element){
-	for (int i=0;i<size;i++){
-	    if ((data[i])==(element)){
-		String[] x=new String[size-1];
-		for (int t=0;t<i;t++){
-		    x[t]=data[t];
-		}
-		for (int y=i+1;y<size;y++){
-		    x[y-1]=data[y];
-		}
-		data=x;
-		size--;
-		return true;
+	int index = -1;
+	for (int x = 0; x < size(); x++){
+	    if (data[x].equals(element) && index == -1){
+		index = x;
 	    }
 	}
-	return false;
+	if (index == -1){
+	    return false;
+	}
+	else {
+	    for (int x = index; x < size(); x++){
+		data[x] = data[x+1];
+	    }
+	    remove(size());
+	    return true;
+	}
+	
     }
+	
 }
