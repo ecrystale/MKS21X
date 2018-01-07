ea2634a2bd0e7609bde43c2712f45ce0b46f6624
Emily <elee19@stuy.edu>
2018-01-06 22:17:03 -0500

    fix orderedsuperarray add

--- 02OrderedSuperArray/OrderedSuperArray.java
+++ 02OrderedSuperArray/OrderedSuperArray.java
@@ -1,33 +1,61 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.*;
 public class OrderedSuperArray extends SuperArray{
     /*constructor initializes an empty List*/
-    public static void main(String[] args){
-    }
-    String x;
     public OrderedSuperArray(){
-	x= super.toString();
+	super();
 	//Complete this.
     }
-
-    /*call the proper add.*/
-    public void add(int index, String value){
-	super.add(index, value);
+    public OrderedSuperArray(int capacity){
+	super(capacity);
+    }
+    public OrderedSuperArray(String[] ary){
+	super(ary.length);
+	for (int i=0;i<ary.length;i++){
+	    add(ary[i]);
+	}
     }
 
+    public String set(int index, String element){
+	throw new UnsupportedOperationException();
+    }
+
+    private int findIndex(String str){
+	for (int i=0;i<size();i++){
+	    if (get(i).compareTo(str)>=0){
+	        return i;
+	    }
+	}
+	return size();
+    }
+
+    private int findIndexBinary(String str){
+	int x=size();
+	for (int i=size()/2-1;x>1;i=x){
+	    if (get(i).compareTo(str)>0){
+		x=i/2;
+	    }
+	    if (get(i).compareTo(str)<0){
+		x=i+i/2;
+	    }
+	    if (get(i).compareTo(str)==0){
+		return i;
+	    }
+	}
+	return x;
+    }
+    /*call the proper add.*/
+    public void add(int index, String value){
+	add(value);
+    }
+    
     /*Write this method and any
       appropriate helper methods.*/
     public boolean add(String value){
-	for (int i=0;i<x.length();i++){
-	    if (x.substring(i,i+1).compareTo(value)==0){
-		add(i,value);
-	    }
-	    if (x.substring(i,i+1).compareTo(value)<0){
-		add(i,value);
-	    }
-	    //add to the correct spot.
-	    //you may still use super.add(index,value)
-	}
+	super.add(findIndex(value), value);
 	return true;
-
     }
 
 }
